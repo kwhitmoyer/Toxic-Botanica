85afefd7ca13fd50bf7984e10b657993ff0152e1
Katherine Whitmoyer <katewhitmoyer@gmail.com>
2024-11-21 12:19:14 -0500

    Initial Commit

---
+++ Toxic Botanica/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Toxic Botanica
+//
+//  Created by Katherine Whitmoyer on 11/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Toxic BotanicaTests/Toxic_BotanicaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Toxic_BotanicaTests.swift
+//  Toxic BotanicaTests
+//
+//  Created by Katherine Whitmoyer on 11/21/24.
+//
+
+import Testing
+@testable import Toxic_Botanica
+
+struct Toxic_BotanicaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
