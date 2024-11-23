dc4fb37b8452185888f8eca8cdf4e3987483285b
Katherine Whitmoyer <katewhitmoyer@gmail.com>
2024-11-23 14:59:11 -0500

    Added ability to switch to detail plant screen

---
+++ Toxic Botanica/DetailViewController.swift
@@ -0,0 +1,16 @@
+//
+//  DetailViewController.swift
+//  Toxic Botanica
+//
+//  Created by Katherine Whitmoyer on 11/21/24.
+//
+
+import UIKit
+
+class PlantDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
--- Toxic Botanica/ViewController.swift
+++ Toxic Botanica/ViewController.swift
@@ -7,11 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        tableView.dataSource = self
+        tableView.delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ReusableCell", for: indexPath)
+        cell.textLabel?.text = "I am one cell!"
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        performSegue(withIdentifier: "toPlantDetail", sender: indexPath)
+    }
+    
+    
+    override func prepare (for segue: UIStoryboardSegue, sender: Any?){
+        if segue.identifier == "toPlantDetail"{
+            if let indexPath = tableView.indexPathForSelectedRow {
+                let destinationVC = segue.destination as? PlantDetailViewController
+            
+            }
+        }
     }
 
 
